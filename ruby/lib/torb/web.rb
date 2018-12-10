235bd56311e8e0ddda4f3fad7173c07693457728
naoki-a-shimizu <naoki.a.shimizu@dena.com>
2018-12-10 17:40:10 +0900

    不要な行を削除

--- ruby/lib/torb/web.rb
+++ ruby/lib/torb/web.rb
@@ -573,7 +573,7 @@ module Torb
 
     get '/admin/api/reports/events/:id/sales', admin_login_required: true do |event_id|
       event = get_event(event_id)
-      keys = %i[reservation_id event_id rank num price user_id sold_at canceled_at]
+
       reservations = db.xquery("SELECT r.id AS reservation_id, r.event_id AS event_id, r.sheet_id, r.user_id, s.rank AS rank, s.num AS num, s.price AS sheet_price, e.id, e.price AS event_price, e.price + s.price AS price, IF(r.canceled_at, DATE_FORMAT(r.canceled_at, '%Y-%m-%dT%TZ'), '') AS canceled_at, DATE_FORMAT(r.reserved_at, '%Y-%m-%dT%TZ') AS sold_at FROM reservations r INNER JOIN sheets s ON s.id = r.sheet_id INNER JOIN events e ON e.id = r.event_id WHERE r.event_id = #{event['id']} ORDER BY r.reserved_at ASC FOR UPDATE")
       render_report_csv(reservations)
     end
